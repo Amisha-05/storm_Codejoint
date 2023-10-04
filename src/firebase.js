2361b8c6189e6b15da703393f06d91c3a76c26e5
Rajeshwari <111358918+starrynightmoo@users.noreply.github.com>
2023-10-04 11:35:56 +0530

    initial commit

--- src/firebase.js
+++ src/firebase.js
@@ -12,12 +12,13 @@ import "firebase/compat/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: "AIzaSyC5uvEVrMvaEZzcO7QrV8u8oJZQXbgF6gs",
-	authDomain: "shewin-40205.firebaseapp.com",
-	projectId: "shewin-40205",
-	storageBucket: "shewin-40205.appspot.com",
-	messagingSenderId: "604340215105",
-	appId: "1:604340215105:web:082a7c4ea18084346515c0",
+	apiKey: "AIzaSyALhR8mokgDt8N4C5ZfRHvO9_B-y35ZvDM",
+  authDomain: "codeissance-b7920.firebaseapp.com",
+  projectId: "codeissance-b7920",
+  storageBucket: "codeissance-b7920.appspot.com",
+  messagingSenderId: "249700418148",
+  appId: "1:249700418148:web:f1389738bce979a55cd571",
+  measurementId: "G-YZJEPDJ3BJ"
 };
 
 // Initialize Firebase
